9198448c4f1053a8bf3f554328ca219c485de1e6
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2023-07-16 22:55:46 +0530

    Create 128.LongestConsecutiveSequence.cpp

---
+++ Day4/128.LongestConsecutiveSequence.cpp
@@ -0,0 +1,28 @@
+//Leetcode link: https://leetcode.com/problems/longest-consecutive-sequence/
+
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        if(n==0)
+            return 0;
+        int count=1,maxi=1;
+    int prev=nums[0];
+
+            for(int i=1;i<n;i++){
+                if(nums[i]==prev+1){
+                    count++;
+                }
+                else if(nums[i]!=prev){
+                   count=1;
+                }
+                prev=nums[i];
+                maxi=max(maxi,count);
+            }
+
+return maxi;
+
+    }
+};
