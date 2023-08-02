b2abefa356e4ada8f162f0800dfecf7f3493d01f
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2023-08-02 23:52:26 +0530

    Update 206.ReverseLinkedList.cpp

--- Day5/206.ReverseLinkedList.cpp
+++ Day5/206.ReverseLinkedList.cpp
@@ -1,14 +1,9 @@
 //Leetcode link: https://leetcode.com/problems/reverse-linked-list/description/
-
 class Solution {
-
 public:
-
     ListNode* reverseList(ListNode* head) {
-    
-        ListNode *temp=NULL;
-
-        while(head!=NULL){
+       ListNode *temp=NULL;
+       while(head!=NULL){
             ListNode *next=head->next;
             head->next=temp;
             temp=head;
