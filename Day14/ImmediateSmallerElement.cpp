220b3cd910519780e865879c1a514d7b37b11244
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2023-09-18 23:20:05 +0530

    Update ImmediateSmallerElement.cpp

--- Day14/ImmediateSmallerElement.cpp
+++ Day14/ImmediateSmallerElement.cpp
@@ -1,8 +1,8 @@
 //Coding ninjas link: https://www.codingninjas.com/studio/problems/immediate-smaller-element-_1062597?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTab=0
 
+
 void immediateSmaller(vector<int>& a)
-{
-	// Write your code here.
+{ // Write your code here.
 	int n=a.size();
 	for(int i=0;i<n-1;i++){
 		if(a[i]>a[i+1]){
